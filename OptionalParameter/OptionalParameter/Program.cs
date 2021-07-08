6f5e9fabf295e89e1c841230f0617a9df075a48c
vishakhshetty <57668627+vishakhshetty@users.noreply.github.com>
2021-07-08 10:33:44 +0530

    Add files via upload

---
+++ OptionalParameter/OptionalParameter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class Program
+    {
+        public void OrderDetails(string SellerName, string ProductName, int Quantity = 1, bool Returnable = true)
+        {
+            Console.WriteLine("Here is the order detail – {0} number of {1} by {2} is ordered. It’s returnable status is {3}\n", Quantity, ProductName, SellerName, Returnable);
+        }
+
+        static void Main(string[] args)
+        {
+
+            Program optinal = new Program();
+            optinal.OrderDetails("Amazon", "Realme Mobile");
+            optinal.OrderDetails("Amazon", "Realme Mobile", 6, false);
+            Console.ReadLine();
+
+        }
+    }
+}
+
